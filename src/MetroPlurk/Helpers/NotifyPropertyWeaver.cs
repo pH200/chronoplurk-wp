9e90101f00ad653beb313381acbe9cabb187bf65
Ting-Yu Lin <disawake@gmail.com>
2011-07-15 17:44:02 -0800

    Updated NPW and moved NPW attribute class to local codes. Added HtmlAgilityPack.

---
+++ src/MetroPlurk/Helpers/NotifyPropertyWeaver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NotifyPropertyWeaver
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class NotifyPropertyAttribute : Attribute
+    {
+        public bool PerformEqualityCheck { get; set; }
+        public string[] AlsoNotifyFor { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class NotifyForAllAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class DoNotNotifyAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
+    public class DependsOnAttribute : Attribute
+    {
+        public DependsOnAttribute(string dependency, params string[] otherDependencies)
+        {
+        }
+    }
+}
