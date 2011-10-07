3cedd355d119a8d0bf8b00c9b66f372e22174eed
Ting-Yu Lin <disawake@gmail.com>
2011-10-07 05:19:54 -0800

    Fix ProgressService.

--- src/ChronoPlurk/Services/ProgressService.cs
+++ src/ChronoPlurk/Services/ProgressService.cs
@@ -1,43 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls.Primitives;
 using ChronoPlurk.Helpers;
 using ChronoPlurk.Views.ProgressBar;
+using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
 namespace ChronoPlurk.Services
 {
     public interface IProgressService
     {
-        void Show();
-        void Show(string message);
+        void Show(string message = null);
         void Hide();
     }
 
     public class ProgressService : IProgressService
     {
-        private readonly ProgressIndicator _progressIndicator;
+        private PhoneApplicationPage _page;
+        private ProgressIndicator _progressIndicator = new ProgressIndicator() {IsVisible = true};
 
-        public ProgressService()
+        public void Show(string message = null)
         {
-            _progressIndicator = new ProgressIndicator() {IsVisible = true};
-            SystemTray.ProgressIndicator = _progressIndicator;
-        }
+            var activePage = GetActivePage();
 
-        public void Show()
-        {
-            Show(null);
-        }
+            if (activePage != null)
+            {
+                if (_page != null && activePage != _page)
+                {
+                    SystemTray.SetProgressIndicator(_page, null);
+                }
 
-        public void Show(string message)
-        {
-            _progressIndicator.Text = message;
-            _progressIndicator.IsIndeterminate = true;
+                _page = activePage;
+                _progressIndicator.Text = message;
+                _progressIndicator.IsIndeterminate = true;
+                SystemTray.SetProgressIndicator(_page, _progressIndicator);
+            } 
         }
 
         public void Hide()
         {
             _progressIndicator.IsIndeterminate = false;
             _progressIndicator.Text = null;
+        }
+
+        private static PhoneApplicationPage GetActivePage()
+        {
+            var currentApplication = Application.Current;
+            if (currentApplication != null)
+            {
+                var rootVisual = currentApplication.RootVisual as PhoneApplicationFrame;
+                if (rootVisual != null)
+                {
+                    return rootVisual.Content as PhoneApplicationPage;
+                }
+            }
+            return null;
         }
     }
 }
