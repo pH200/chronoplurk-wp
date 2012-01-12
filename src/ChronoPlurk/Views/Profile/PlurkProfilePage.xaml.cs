7a4da66e062d5822e0f3a909e0a8dbb01f01c863
Ting-Yu Lin <disawake@gmail.com>
2012-01-12 14:22:22 -0800

    Localize PlurkProfilePage.xaml.

--- src/ChronoPlurk/Views/Profile/PlurkProfilePage.xaml.cs
+++ src/ChronoPlurk/Views/Profile/PlurkProfilePage.xaml.cs
@@ -9,7 +9,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Caliburn.Micro;
+using ChronoPlurk.Helpers;
+using ChronoPlurk.Resources.i18n;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace ChronoPlurk.Views.Profile
 {
@@ -18,6 +22,23 @@ namespace ChronoPlurk.Views.Profile
         public PlurkProfilePage()
         {
             InitializeComponent();
+            BuildAppBar();
+        }
+
+        private void BuildAppBar()
+        {
+            ApplicationBar = new ApplicationBar()
+            {
+                BackgroundColor = PlurkResources.PlurkColor
+            };
+            var refreshButton = new AppBarButton()
+            {
+                IconUri = new Uri("Resources/Icons/appbar.refresh.rest.png", UriKind.Relative),
+                Text = AppResources.appbarRefresh,
+                Message = "RefreshAppBar"
+            };
+
+            ApplicationBar.Buttons.Add(refreshButton);
         }
     }
 }
