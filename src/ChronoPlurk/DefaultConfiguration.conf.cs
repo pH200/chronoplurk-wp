96483c5effab0f6c66da6f57359f9c24a6f4eb70
pH200 <ph.minamo@cytisan.com>
2017-05-07 20:19:42 +0700

    Include configuration file

---
+++ src/ChronoPlurk/DefaultConfiguration.conf.cs
@@ -0,0 +1,14 @@
+namespace ChronoPlurk
+{
+    public static partial class DefaultConfiguration
+    {
+        static DefaultConfiguration()
+        {
+            OAuthConsumerKey =
+""; // %%omit oauth_consumer_key%%
+
+            OAuthConsumerSecret =
+""; // %%omit oauth_consumer_secret%%
+        }
+    }
+}
--- src/plurto/Plurto.Test/TestConfigStrings.cs
+++ src/plurto/Plurto.Test/TestConfigStrings.cs
@@ -19,52 +19,5 @@ namespace Plurto.Test
         public static string OAuthAccessToken;
 
         public static string OAuthAccessTokenSecret;
-
-        static TestConfigStrings()
-        {
-            if (ApiKey == null)
-            {
-                Func<string, string> read = (name) =>
-                {
-                    if (File.Exists("./" + name))
-                    {
-                        return File.ReadAllText("./" + name);
-                    }
-                    else if (File.Exists("../" + name))
-                    {
-                        return File.ReadAllText("../" + name);
-                    }
-                    else if (File.Exists("../../" + name))
-                    {
-                        return File.ReadAllText("../../" + name);
-                    }
-                    else if (File.Exists("../../../" + name))
-                    {
-                        return File.ReadAllText("../../../" + name);
-                    }
-                    else if (File.Exists("../../../../" + name))
-                    {
-                        return File.ReadAllText("../../../../" + name);
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                };
-                var json = read("variables.omitconf.json");
-                if (json == null)
-                {
-                    return;
-                }
-                var obj = JObject.Parse(json);
-                ApiKey = (string)obj["ApiKey"];
-                Username = (string)obj["Username"];
-                Password = (string)obj["Password"];
-                OAuthConsumerKey = (string)obj["OAuthConsumerKey"];
-                OAuthConsumerSecret = (string)obj["OAuthConsumerSecret"];
-                OAuthAccessToken = (string)obj["OAuthAccessToken"];
-                OAuthAccessTokenSecret = (string)obj["OAuthAccessTokenSecret"];
-            }
-        }
     }
 }
